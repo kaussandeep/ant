67118d6fadbfec11b271aa19064f765ecee4a215
Sandeep Kaushik <sandeep.kaushik@qlik.com>
2023-02-06 14:33:35 +0100

    chore: add Page Object Models

--- test/firstTest.spec.js
+++ test/firstTest.spec.js
@@ -3,7 +3,10 @@ import { test, expect } from '@playwright/test';
 import { RiskPage } from './page/risk.js'
 import { LoginPage } from './page/login.js'
 
-test('test', async ({ page }) => {
+test.describe("antura", () => {
+
+
+test('Add risk and verify risk is added', async ({ page }) => {
   const loginPage = new LoginPage(page);
   const riskPage = new RiskPage(page);
 
@@ -13,32 +16,10 @@ await loginPage.login();
 
 await page.getByRole('button', { name: 'Huvudmeny' }).click();
 
-  await page.getByRole('button', { name: 'Projekt Kastanjen' }).click( { force: true });
-  await page.getByRole('button', { name: 'Risker' }).click();
-  await page.getByTitle('Ny risk').click();
-  await page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByLabel('Namn:').click();
-await riskPage.fillName("newRisk");
- 
-  await page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByLabel('Namn:').click();
+await page.getByRole('button', { name: 'Projekt Kastanjen' }).click( { force: true });
+await riskPage.clickRisk();
+await riskPage.fillRiskDetails("newRisk");
 
-  await page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByLabel('Namn:').press('Tab');
-  await page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByRole('combobox', { name: 'Sannolikhet (S):' }).selectOption('2');
-  await page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByRole('combobox', { name: 'Konsekvens (K):' }).selectOption('3');
-  await page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByRole('combobox', { name: 'Berörda intressenter:' }).selectOption('33609');
-  await page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByLabel('Kostnad för åtgärd:').click();
-  await page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByLabel('Kostnad för åtgärd:').fill('1200');
-  await page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByLabel('Beskrivning:').click();
-  await page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByLabel('Beskrivning:').fill('example beskrivning');
-  await page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByLabel('Sannolik konsekvenskostnad:').click();
-  await page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByLabel('Sannolik konsekvenskostnad:').fill('1500');
-  await page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByText('Inga').click();
-  await page.getByRole('checkbox', { name: 'Begränsa' }).check();
-  await page.getByRole('checkbox', { name: 'Dela' }).check();
-  await page.getByRole('link', { name: 'Markera alla', exact: true }).click();
-  await page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').locator('html').click();
-  await page.locator('.projectsWindowOverlay').click();
-  await page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').locator('html').click();
-  await page.locator('.projectsWindowOverlay').click();
-  await page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').locator('html').click();
-  await page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').locator('body').press('Escape');
+
+});
 });
--- test/page/risk.js
+++ test/page/risk.js
@@ -1,4 +1,5 @@
-import { page } from '@playwright/test';
+import { expect, page } from '@playwright/test';
+var assert = require('assert');
 
 export class RiskPage {
    constructor(page) {
@@ -6,11 +7,37 @@ export class RiskPage {
       
         
       }
-     
-      async fillName(riskName) {
-        await this.page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByLabel('Namn:').fill(riskName);
-      }
-     
+     async clickRisk(){
+      await this.page.getByRole('button', { name: 'Risker' }).click();
+      await this.page.getByTitle('Ny risk').click();
+     }
+   
+     async fillRiskDetails (riskName){
+      await this.page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByLabel('Namn:').fill(riskName);
+      await this.page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByLabel('Namn:').press('Tab');
+      await this.page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByRole('combobox', { name: 'Status:' }).press('Tab');
+      await this.page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').locator('[id="_taskGroupPanel__taskNameSection__groupPanel__ownerGroupSection__ownerChooser"]').getByRole('link').press('Tab');
+      await this.page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByLabel('Beskrivning:').fill('beskrivning risk');
+      await this.page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByLabel('Beskrivning:').press('Tab');
+      await this.page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByRole('link', { name: 'Riskvärdesförklaring' }).press('Tab');
+      await this.page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByRole('combobox', { name: 'Sannolikhet (S):' }).press('ArrowDown');
+      await this.page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByRole('combobox', { name: 'Sannolikhet (S):' }).selectOption('3');
+      await this.page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByRole('combobox', { name: 'Sannolikhet (S):' }).press('Tab');
+      await this.page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByRole('combobox', { name: 'Konsekvens (K):' }).selectOption('3');
+      await this.page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByRole('combobox', { name: 'Berörda intressenter:' }).selectOption('33609');
+      await this.page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByLabel('Kostnad för åtgärd:').click();
+      await this.page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByLabel('Kostnad för åtgärd:').fill('10000');
+      await this.page.frameLocator('iframe[name="\\/Views\\/ProjectPlace\\/Risks\\/EditRisk\\.aspx\\?ProjectID\\=68409\\&OpenedFromMyPlaceRisks\\=False"]').getByRole('button', { name: 'OK' }).click();
+  
+   
+      await expect(this.page.getByText(riskName)).toContainText(riskName);
+      await this.page.getByRole('row', { name: 'ID Risk Beskrivning S K S*K RV Riskvärdesförklaring Åtgärdsklassning Åtgärder Status Ägare Riskhistorik' }).getByRole('checkbox').check();
+      await this.page.getByTitle('Ta bort').click();
+      await this.page.getByRole('link', { name: 'Ja' }).click();
+
+      
+    
+     }
 
 }
 
